e39b4170fdcefd756d2e788158f222d403edb8c3
Michael Eckebrecht <direkt@route-media.info>
2018-10-27 21:13:16 +0200

    Update settings.php

--- cimply/system/settings.php
+++ cimply/system/settings.php
@@ -2,7 +2,7 @@
 namespace Cimply\System {
     interface Settings {
         const SystemPath = __DIR__.DIRECTORY_SEPARATOR;
-        const TempDir = 'C:\\inetpub\\temp\\procesa';
+        const TempDir = self::SystemPath.'tmp';
         const Assembly = [
             'Helper' => self::SystemPath.'Helper',
             'Yaml' => self::SystemPath.'Vendor\\Yaml',
