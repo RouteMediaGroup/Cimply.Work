d5d0b4ea8a703e7b67cc30c81503b8068e21171f
Michael Eckebrecht <direkt@route-media.info>
2019-12-05 23:37:42 +0100

    Update
    Add constructor with table-set info

--- cimply/core/model/entitybase.php
+++ cimply/core/model/entitybase.php
@@ -3,6 +3,11 @@ namespace Cimply\Core\Model {
     class EntityBase
     {
         use \Properties, \Cast;
+        public $table, $infoMessage = array(), $saveAble = true, $refresh = false;
+
+        function __construct($table = null) {
+            isset($table) ? $this->table = $table : null;
+        }
 
         /**
          * Summary of Cast
@@ -13,9 +18,5 @@ namespace Cimply\Core\Model {
         static function Cast($mainObject, $selfObject = self::class): self {
             return static::Cull($mainObject, $selfObject, true);
         }
-
-        public $infoMessage = array();
-        public $saveAble = true;
-        public $refresh = false;
     }
 }
