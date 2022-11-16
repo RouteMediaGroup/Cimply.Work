53db4e46f1870e09c5ecfc5badd9f59c777c64ed
Michael Eckebrecht <direkt@route-media.info>
2022-11-16 09:26:09 +0100

    Update files.php
    BugFixes

--- cimply/system/helper/files.php
+++ cimply/system/helper/files.php
@@ -1,23 +1,15 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Summary of namespace 
- */
 namespace
 {
 	/**
-	 * ToDo: Beschreibung von File fehlt!
+	 * @Author Michael Eckebrecht
 	 */
 	trait Files
     {
         static function GetFileContent($filename, $path = false, $options = null, $method = 'GET', $param1 = null, $param2 = null): ?string
         {
+            $currentFile = "";
+            $filename = str_replace('\\',DIRECTORY_SEPARATOR, $filename);
             if(\is_file($filename)) {
                 $opts = isset($options) ? $options : array(
                     'http'=>array(
@@ -28,15 +20,27 @@ namespace
                 $context = stream_context_create($opts);
                 $currentFile = \file_get_contents($filename, $path, $context, $param1) ?? null;
             }
-            return $currentFile ?? null;
+            return !(empty($currentFile)) ? $currentFile : null;
         }
-        
+        static function PutFileContent($filename, $data = "", $options = null, $method = 'POST', $deep = 0): void
+        {
+            if(\is_file($filename)) {
+                $opts = isset($options) ? $options : array(
+                    'http'=>array(
+                        'method'=> $method,
+                        'header'=>"Accept-language: en\r\n" . "Cookie: set=null\r\n"
+                    )
+                );
+                $context = stream_context_create($opts);
+                \file_put_contents($filename, $data, $deep, $context) ?? null;
+            }
+        }
         static function GetFilePath($path = '', $options = null) {
             $result = array();
             isset($path) ? $result = (isset($options) ? \pathinfo($path, $options) : \pathinfo($path)) : null;
             return $result;
         }
-        
+
         static function GetHttpsFile($url, $path = null, $username = "", $password = "", $timeout = 60, $method = 'POST', $param1 = -1, $param2 = 40000)
         {
             $body = '';
@@ -51,45 +55,51 @@ namespace
             );
             self::GetFile($url, $path, $opts, null, $param1, $param2);
         }
-        
-        static function GetUtf8File($fn) { 
-            return mb_convert_encoding($content = self::GetFileContent($fn) ?? '', 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true)); 
-        } 
-        
-        static function SetFileKey($project = 'global', $filename = '') {
-            return md5($project.str_replace(' ','', $filename)); 
+
+        static function GetUtf8File($fn) {
+            return mb_convert_encoding($content = self::GetFileContent($fn) ?? '', 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
         }
-        
+
+        static function SetFileKey($project = 'global', $filename = '') {
+            return md5($project.str_replace(' ','', $filename));
+        }
+
         static function RemoveFile($filePath) {
             try {
                 \unlink($filePath);
             } catch (Exception $ex) {
                 \Debug::VarDump($ex);
-            }; 
+            };
         }
 
-        static function FileForceContents($filename = null, $data, $flags = 0) {
-            /*
-            $path = "";
-            if(!is_dir(dirname($path))) {
-                mkdir(dirname($path), 0777, TRUE);    
-            }
-            */
-            return (bool)file_put_contents($filename, $data, $flags) ?? false;
+        static function FileForceContents($filename = null, $data, $flags = 0):bool {
+            return (bool)  (\is_file(str_replace('\\',DIRECTORY_SEPARATOR, $filename)) === true) ? (file_put_contents(str_replace('\\',DIRECTORY_SEPARATOR, $filename), $data, $flags) ?? false) : false;
         }
-
-        static function CacheFile($filePath, $fileName) {
+        static function CacheFile($filePath, $cacheFile) {
             \ob_start();
-            require_once($filePath);
+            require($filePath);
             $fileData = ob_get_contents();
             \ob_end_clean();
-           
-            !(isset($fileData)) ? : self::FileForceContents($fileName, $fileData);
+            self::FileForceContents($cacheFile, $fileData, 0);
+            !(isset($fileData)) ? null : self::FileForceContents($cacheFile, $fileData, 0);
             return $fileData;
         }
 
         static function HasFileCached($fileName): bool {
-            return (\is_file($fileName));
+            return (bool)\is_file($fileName);
+        }
+
+        static function DeCryptFile($fileName): void {
+            (string)$tmpFile = ".compile";
+            ob_start();
+            require_once($fileName);
+            (string)$cryptedData = ob_get_contents();
+            \ob_end_clean();
+            $splitData = \explode('=',$cryptedData);
+            (string)$code_base64 = Crypto::Decrypt($splitData[0].'=','AES-256-CBC',end($splitData));
+            if(self::FileForceContents($tmpFile,$code_base64,0)) {
+                require_once($tmpFile);
+            }
         }
     }
 }
