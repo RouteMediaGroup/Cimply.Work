cf5385e23357c3955e3eb9e732e99968ae233e23
Michael Eckebrecht <direkt@route-media.info>
2026-02-26 13:08:55 +0100

    Refactor Translate class for improved type safety

--- cimply/core/view/translate.php
+++ cimply/core/view/translate.php
@@ -1,78 +1,145 @@
 <?php
+/*
+ * Cimply.Work - Business Framework 2012-2025: Proprietary commercial license © RouteMedia® – Represented by Michael Eckebrecht.
+ * Contact: direkt@route-media.info. All rights reserved.
+*/
 
 namespace Cimply\Core\View {
 
-    /**
-     * Description of Cimply.Work
-     *
-     * @author Michael Eckebrecht
-     */
-    use \Cimply\System\Helpers as Helper;
+    use Cimply\System\Helpers as Helper;
+
     class Translate
     {
         use \Properties, \Cast;
 
-        protected static $pattern = null; 
-        function __construct($pattern = null) {
-            self::$pattern = $pattern;
+        protected static ?array $pattern = null;
+
+        public function __construct($pattern = null)
+        {
+            self::$pattern = \is_array($pattern) ? $pattern : null;
         }
 
-        public final function Cast($mainObject, $selfObject = self::class): self {
+        public final function Cast($mainObject, $selfObject = self::class): self
+        {
             return self::Cull($mainObject, $selfObject);
         }
 
-        function OnPropertyChanged() {
+        public function OnPropertyChanged(): void
+        {
             self::$staticProperties = $this;
         }
 
-        //Get Translation
-        static function GetTranslastion(string $s = "", $langCode = 'de_DE') {
+        private static function normalizeToString($value): string
+        {
+            if ($value === null) {
+                return '';
+            }
+            if (\is_string($value) || \is_int($value) || \is_float($value) || \is_bool($value)) {
+                return (string)$value;
+            }
+            if (\is_object($value) && \method_exists($value, '__toString')) {
+                return (string)$value;
+            }
+            if (\is_array($value)) {
+                $json = \json_encode($value, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+                return $json !== false ? $json : '';
+            }
+            $json = \json_encode($value, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            return $json !== false ? $json : '';
+        }
+
+        // Get Translation
+        public static function GetTranslastion($s = "", $langCode = 'de_DE'): string
+        {
+            $s = self::normalizeToString($s);
+
+            $pattern = self::$pattern['Trans'] ?? null;
+            if (!\is_string($pattern) || $pattern === '') {
+                return $s;
+            }
+
             $matches = [];
-            @preg_match_all(self::$pattern['Trans'], $s, $matches);
-            if(isset($matches[1])) {
-                $i = 0;
-                foreach($matches[1] as $key => $value) {
-                    if($trans = self::WordTranslation($matches[1][$i], $langCode)) {
-                        $s = str_replace($matches[0][$i], $trans, $s);
-                    }
-                    $i++;
+            \preg_match_all($pattern, $s, $matches);
+
+            if (!isset($matches[1]) || !\is_array($matches[1]) || \count($matches[1]) === 0) {
+                return $s;
+            }
+
+            foreach ($matches[1] as $i => $value) {
+                $trans = self::WordTranslation($value, $langCode);
+                if ($trans !== null && $trans !== '') {
+                    $s = \str_replace($matches[0][$i], $trans, $s);
                 }
             }
+
             return $s;
         }
 
-        static function WordTranslation($value, $langCode = 'de_DE') {
+        public static function WordTranslation($value, $langCode = 'de_DE'): string
+        {
+            $value = self::normalizeToString($value);
+
             $translation = (array)self::GetStaticProperty('Translations');
-            $checkString = $translation[$value] ?? $translation;
-            return $checkString[$langCode] ?? $value;
+
+            if (\array_key_exists($value, $translation)) {
+                $entry = $translation[$value];
+                if (\is_array($entry) && \array_key_exists($langCode, $entry)) {
+                    return self::normalizeToString($entry[$langCode]);
+                }
+            }
+
+            if (\is_array($translation) && \array_key_exists($langCode, $translation)) {
+                return self::normalizeToString($translation[$langCode]);
+            }
+
+            return $value;
         }
 
-        static function Translation($value, $trans = null): string {
-            !(isset($trans)) ? $trans = self::GetStaticProperty('Translastions') : null;
+        public static function Translation($value, $trans = null): string
+        {
+            if ($trans === null) {
+                $trans = self::GetStaticProperty('Translastions');
+            }
+
             $translation = self::GetTranslastion($value);
-            //die(\var_dump(self::GetTranslastion($value)));
-            //count($translation) === 0 ? $translation = $value : null;
-            $explString = \explode(" ", $translation);
-            $counts = strlen(end($explString));
-            //die(\var_dump($explString[2]));
+
+            $explString = \explode(' ', $translation);
+            $last = \end($explString);
+            $counts = \strlen((string)$last);
+
             $replacesWord = [];
-            foreach($checkString = end($trans) as $key => $val) {
-                if(substr($key, 0, $counts) === $explString[0]) {
-                    if(similar_text($key, $translation, $percent)) {
-                        if($percent >= 50) {
-                            $trimResult = explode(" ", $key);
-                            foreach ($trimResult as $k => $v) {
-                                if(isset($explString[$k]) && ($v != $explString[$k])) {
-                                    $i = str_replace('%', '', $v);
-                                    $replacesWord[$i] = self::WordTranslation($explString[$k]);
-                                }
+
+            $transArray = \is_array($trans) ? $trans : [];
+            $transEnd = \end($transArray);
+
+            if (!\is_array($transEnd)) {
+                return $translation;
+            }
+
+            foreach ($transEnd as $key => $val) {
+                $keyStr = self::normalizeToString($key);
+
+                if ($counts > 0 && \substr($keyStr, 0, $counts) === ($explString[0] ?? '')) {
+                    $percent = 0.0;
+                    if (\similar_text($keyStr, $translation, $percent) && $percent >= 50) {
+                        $trimResult = \explode(' ', $keyStr);
+
+                        foreach ($trimResult as $k => $v) {
+                            if (isset($explString[$k]) && ($v != $explString[$k])) {
+                                $i = \str_replace('%', '', (string)$v);
+                                $replacesWord[$i] = self::WordTranslation($explString[$k]);
                             }
-                            $translation = \vsprintf(end($val), $replacesWord);
+                        }
+
+                        $valEnd = \end($val);
+                        if (\is_string($valEnd) && $valEnd !== '') {
+                            $translation = \vsprintf($valEnd, $replacesWord);
                         }
                     }
                 }
             }
-            return ($translation);
+
+            return $translation;
         }
     }
 }
