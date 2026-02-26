552871c052a96952a99f8da5cd56c4252b178795
Michael Eckebrecht <direkt@route-media.info>
2026-02-26 12:58:43 +0100

    Refactor GetAnnotations method in Annotation trait
    Refactor GetAnnotations method to handle classObject as a string and improve exception handling.

--- cimply/system/helper/annotation.php
+++ cimply/system/helper/annotation.php
@@ -1,17 +1,29 @@
 <?php
-namespace {   
+/*
+ * Cimply.Work - Business Framework 2012-2025: Proprietary commercial license © RouteMedia® – Represented by Michael Eckebrecht.
+ * Contact: direkt@route-media.info. All rights reserved.
+*/
+
+namespace {
     use Cimply\Core\Annotation\Annotation as Annotate;
+
     trait Annotation {
 
-        public static function GetAnnotations($classObject = null): Annotate {
-            $objectExpl = explode("::", $classObject);
+        public static function GetAnnotations($classObject = null): Annotate
+        {
+            $classObject = $classObject !== null ? (string)$classObject : '';
+
+            $objectExpl = \explode("::", $classObject);
             $className = $objectExpl[0] ?? null;
-            $method = $objectExpl[1] ?? null;;
+            $method = $objectExpl[1] ?? null;
+
             try {
                 $annotate = new Annotate($className, $method);
-            } catch (\Exception | \ReflectionClass | \ReflectionMethod | \ReflectionProperty | \AnnotateException | ArgumentCountError $e) {
+            } catch (\Exception | \ReflectionException | \AnnotateException | \ArgumentCountError $e) {
                 \Debug::VarDump($e->getMessage());
-            }		
+                $annotate = new Annotate(null, null);
+            }
+
             return $annotate;
         }
     }
